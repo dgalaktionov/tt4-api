642250b344f4773e9c2d9c653a717325f83a9b16
DaGal <dgalaktionov@enxenio.es>
2016-05-10 16:55:39 +0200

    Database

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-var pg = require('pg');
+var promise = require('bluebird');
+
+var options = {
+  // Initialization Options
+  promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = process.env.DATABASE_URL || "postgres://ymwtkpslgvcdsd:ZLDz1JNyk8Io6PwWZkXLkYyO0D@ec2-54-225-79-232.compute-1.amazonaws.com:5432/dsnji1r8g5f2r";
+var db = pgp(connectionString);
 var express = require('express');
 var bodyParser = require("body-parser");
 var app = express();
---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE points (
+  ID SERIAL PRIMARY KEY,
+  nombre VARCHAR,
+  lat REAL,
+  lon REAL
+);
