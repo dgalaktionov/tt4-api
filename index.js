b926f03dfd373a6f40bf730b4cc37ccb07aa020f
DaGal <dgalaktionov@enxenio.es>
2016-05-10 17:14:31 +0200

    get and post requests

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = process.env.DATABASE_URL || "postgres://ymwtkpslgvcdsd:ZLDz1JNyk8Io6PwWZkXLkYyO0D@ec2-54-225-79-232.compute-1.amazonaws.com:5432/dsnji1r8g5f2r";
+var connectionString = process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/tt4";
 var db = pgp(connectionString);
 var express = require('express');
 var bodyParser = require("body-parser");
@@ -17,8 +17,31 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(request, response) {
-  response.send("Hello World");
+function defaultAnswer(request, response) {
+	response.send("Hello World");
+}
+
+app.get('/', defaultAnswer);
+
+app.get('/points', function (req, res) {
+  db.any('select * from points')
+    .then(function (data) {
+      res.status(200)
+        .json(data);
+    });
+});
+
+app.post('/points', function (req, res) {
+  db.none('insert into points(nombre, lat, lon)' +
+      'values(${nombre}, ${lat}, ${lon})',
+    req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Inserted one point'
+        });
+    })
 });
 
 app.listen(app.get('port'), function() {
